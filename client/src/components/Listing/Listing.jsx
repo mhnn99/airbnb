83c5628716a35f84318b6a4b1744ff2b9feda61f
Radu Zarafin <mihneabiceps3@gmail.com>
2023-05-07 14:48:04 +0300

    added swiper

--- client/src/components/Listing/Listing.jsx
+++ client/src/components/Listing/Listing.jsx
@@ -4,9 +4,19 @@ import React, { useState } from "react";
 import { Typography } from "@mui/material";
 import { useEffect } from "react";
 import { Grid, Box } from "@mui/material";
-import Carousel from 'react-material-ui-carousel'
+import Carousel from "react-material-ui-carousel";
+import { useTheme } from "@mui/material";
+import {Swiper, SwiperSlide} from 'swiper/react'
+import {Navigation, Autoplay, EffectCoverflow, Zoom} from 'swiper'
+import 'swiper/css'
+import 'swiper/css/navigation'
+import 'swiper/css/effect-coverflow'
+import 'swiper/css/zoom'
+import Container from '@mui/material/Container'
+
 
 const Listing = () => {
+  const theme = useTheme();
   const [listing, setListing] = useState([]);
   const { id } = useParams();
   const listings = useSelector((state) => state.listings);
@@ -17,33 +27,39 @@ const Listing = () => {
 
   return (
     <>
-      {listing.length > 0 && <Typography>{listing[0].name}</Typography>}
+      {listing.length > 0 && (
+        <Typography sx={{ fontSize: "26px" }}>{listing[0].name}</Typography>
+      )}
       <Box sx={{ marginTop: 4 }}>
-        <Grid container spacing={3}>
-          <Grid item xs={12} md={6}>
-            details
-          </Grid>
+          <Container>
+            <Swiper
+            modules={[Navigation,Autoplay,EffectCoverflow,Zoom]}
+            centeredSlides
+            slidesPerView={2}
+            grabCursor
+            navigation
+            autoplay
+            zoom
+            effect="coverflow"
+            coverflowEffect={{
+              rotate:50,
+              stretch:0,
+              depth:100,
+              modifier:1,
+              slideShadows:true
+            }}>
+
           {listing[0]?.images && listing[0]?.images.length > 0 && (
-            <Grid item xs={12} md={6}>
-              <Carousel
-                    autoPlay={true}
-                  >
-                    {listing[0].images.map((item, index) => (
-                      <img
-                        key={index}
-                        src={item}
-                        alt="app pic"
-                        style={{
-                          display: 'block',
-                          maxWidth: '100%',
-                          height: 'auto',
-                        }}
-                      />
-                    ))}
-                  </Carousel>
-            </Grid>
+            listing[0].images.map((image,i)=>(
+              <SwiperSlide>
+                <div>
+                  <img src={image} alt='asdf' style={{maxHeight:'400px', width:"100%", objectFit:'cover'}}/>
+                </div>
+              </SwiperSlide>
+            ))
           )}
-        </Grid>
+            </Swiper>
+          </Container>
       </Box>
     </>
   );
