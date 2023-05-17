af3ec18b6a036425117014572f1ec33514a7bd87
Radu Zarafin <mihneabiceps3@gmail.com>
2023-05-17 15:18:38 +0300

    favorites

--- client/src/components/Account/Account.jsx
+++ client/src/components/Account/Account.jsx
@@ -1,5 +1,4 @@
-import React from "react";
-import { useState, useEffect } from "react";
+import React, { useState, useEffect, useMemo } from "react";
 import { useSelector } from "react-redux";
 import Navbar from "../Navbar/Navbar";
 import { Card, Typography, Grid } from "@mui/material";
@@ -13,42 +12,62 @@ const Account = () => {
     mode: state.mode,
   }));
   const favorites = useSelector((state) => state.favorites);
-  const groupedFavs = favorites.flat().reduce((a, fav) => {
-    const key = fav.city;
-    if (a[key] == null) a[key] = [];
-    a[key].push(fav.favorites);
-    return a;
-  }, {});
+  
+  const groupedFavs = useMemo(() => {
+    return favorites.flat().reduce((a, fav) => {
+      const key = fav.city;
+      if (a[key] == null) a[key] = [];
+      a[key].push(fav.favorites);
+      return a;
+    }, {});
+  }, [favorites]);
+
+  const [favCities, setFavCities] = useState([]);
+  const [isLoading, setIsLoading] = useState(true);
+  const [error, setError] = useState(null);
+
   useEffect(() => {
     const fetchCities = async () => {
-      const favCities = await Promise.all(
-        Object.keys(groupedFavs).map(async (url) => {
-          const response = await fetch(
-            `https://airbnb13.p.rapidapi.com/search-location?location=${url}&checkin=2023-09-16&checkout=2023-09-17&adults=1&children=0&infants=0&pets=0&page=1&currency=USD`,
-            {
-              method: "GET",
-              headers: {
-                "X-RapidAPI-Key": "f6dfa6fc95msh3ea6d12e80670ecp18a7dfjsne8f981e298ac",
-                "X-RapidAPI-Host": "airbnb13.p.rapidapi.com",
-              },
-            }
-          );
-          const data = await response.json();
-          return { [url]: data.results }; // Append the `url` to the response data
-        })
-      );
-      console.log(favCities);
+      try {
+        const fetchedCities = await Promise.all(
+          Object.entries(groupedFavs).map(async ([key, value]) => {
+            const response = await fetch(
+              `https://airbnb13.p.rapidapi.com/search-location?location=${key}&checkin=2023-09-16&checkout=2023-09-17&adults=1&children=0&infants=0&pets=0&page=1&currency=USD`,
+              {
+                method: "GET",
+                headers: {
+                  "X-RapidAPI-Key": "f6dfa6fc95msh3ea6d12e80670ecp18a7dfjsne8f981e298ac",
+                  "X-RapidAPI-Host": "airbnb13.p.rapidapi.com",
+                },
+              }
+            );
+            const data = await response.json();
+            return { city: key, fav: data.results.filter((listing) => value.includes(listing.id)) };
+          })
+        );
+
+        setFavCities(fetchedCities.reduce((a, b) => {
+          const key = b.city;
+          if (a[key] == null) a[key] = [];
+          a[key] = b.fav;
+          return a;
+        }, {}));
+        setIsLoading(false);
+      } catch (err) {
+        setError(err.message);
+        setIsLoading(false);
+      }
     };
+
     fetchCities();
   }, [groupedFavs]);
-  
-  console.log(groupedFavs);
+
   return (
     <>
       <Navbar />
       <Box sx={{ m: 5 }}>
         <Typography variant="h3" align="left">
-          Hello, {isLoggedIn.user.firstName} !
+          Hello, {isLoggedIn.user.firstName}!
         </Typography>
         <Grid item xs={12} sm={6}>
           <TextField
@@ -60,8 +79,25 @@ const Account = () => {
           />
         </Grid>
       </Box>
-    </>
-  );
-};
+      {isLoading ? (
+        <div>Loading...</div>
+      ) : error ? (
+        <div>Error: {error}</div>
+      ) : (
+        <div>
+          {Object.entries(favCities).map(([city, fav]) => (
+            <div key={city}>
+              <h3>{city}</h3>
+              {fav.map((listing) => (
+                <Card key={listing.id}>
+                  {/* Render your listing card here */}
+                </Card>
+              ))}
+            </div>
+          ))}
+        </div>
+      )}
+</>
+  )}
 
-export default Account;
+  export default Account
