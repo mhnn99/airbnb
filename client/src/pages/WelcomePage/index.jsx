2b7a46a2bd1cc1bcd06f56126aa18fffb8bd81b8
Radu Zarafin <mihneabiceps3@gmail.com>
2023-05-18 15:01:33 +0300

    parallax

--- client/src/pages/WelcomePage/index.jsx
+++ client/src/pages/WelcomePage/index.jsx
@@ -4,6 +4,8 @@ import Snackbar from '@mui/material/Snackbar';
 import MuiAlert from '@mui/material/Alert';
 import { useSelector } from "react-redux";
 import CatList from "../../components/Catlist/CatList";
+import { Parallax, ParallaxLayer } from "@react-spring/parallax";
+import { Box, Typography } from "@mui/material";
 const WelcomePage = () =>{
     const [open,setOpen] = useState(true)
     const Alert = React.forwardRef(function Alert(props, ref) {
@@ -17,7 +19,17 @@ const WelcomePage = () =>{
       Succesfully logged in!
     </Alert>
   </Snackbar>}
+  <Parallax pages={2}>
+<ParallaxLayer offset={0} speed={1}
+style={{backgroundImage:`url(https://images.pexels.com/photos/2356045/pexels-photo-2356045.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1)`,backgroundSize:'cover'}}>
+<Box >
+<Typography sx={{fontSize:'10rem', textAlign:'center'}} color='primary'>Welcome to our travel app</Typography>
+</Box>
+</ParallaxLayer>
+<ParallaxLayer offset={1} speed={0.5}>
   <CatList/>
+</ParallaxLayer>
+  </Parallax>
     </>
 }
 
